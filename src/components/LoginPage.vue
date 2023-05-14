9b7c2545a1cd03ff9389f9595962b44aa82c21e2
flobrflo <florian.duc@hotmail.fr>
2023-05-14 02:06:47 +0200

    update login form

--- src/components/LoginPage.vue
+++ src/components/LoginPage.vue
@@ -1,10 +1,18 @@
 <template>
-  <div class="login">
+  <form @submit="login" class="login">
     <h2>Login</h2>
-    <input type="text" v-model="email" placeholder="Email"><br>
-    <input type="password" v-model="password" placeholder="Password"><br>
-    <button @click="login">Sign in</button>
-  </div>
+    <div class="form-row">
+      <input type="email" v-model="email" placeholder="Email" required>
+    </div>
+
+    <div class="form-row">
+      <input type="password" v-model="password" placeholder="Password" required>
+    </div>
+
+    <div class="form-row">
+      <input type="submit" value="Sign In" class="submit-btn"/>
+    </div>
+  </form>
 </template>
 
 <script>
@@ -19,12 +27,15 @@
       };
     },
     methods: {
-      login: function() {
+      login: function(e) {
+        e.preventDefault();
+        e.stopPropagation();
+
         const auth = getAuth();
         signInWithEmailAndPassword(auth, this.email, this.password)
           .then((userCredential) => {
-            // Signed in 
-            console.log('user register :', userCredential.user);
+            // Signed in
+            console.log('user signin :', userCredential.user);
             this.$router.replace("dashboard");
           })
           .catch((error) => {
@@ -40,24 +51,25 @@
 <style scoped>
 /* "scoped" attribute limit the CSS to this component only */
 .login {
-  margin-top: 50px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  margin-top: 40px;
+}
+.form-row {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
 }
 input {
   width: 40%;
   padding: 20px;
   margin: 20px 0;
 }
-button {
+.submit-btn {
   width: 10%;
   margin-top: 30px;
   padding: 20px;
-  cursor: pointer;
-}
-p {
-  margin-top: 50px;
-  font-size: 16px;
-}
-p a {
   cursor: pointer;
 }
 </style>
--- src/components/RegisterPage.vue
+++ src/components/RegisterPage.vue
@@ -1,14 +1,37 @@
 <template>
-  <div class="register">
-    <p>Register</p>
-    <input type="text" v-model="email" placeholder="Email">
-    <br>
-    <input type="password" v-model="password" placeholder="Password">
-    <br>
-    <button @click="register">Register</button>
-    <span><router-link to="/login">login</router-link>.
-    </span>
-  </div>
+  <form @submit="register" class="register">
+    <h2>Register</h2>
+    
+    <div class="form-row">
+      <div class="nomark"></div>
+      <input type="email" v-model="email" placeholder="Email">
+      <div v-if="email_valid === null" class="nomark"></div>
+      <div v-if="email_valid" class="checkmark"></div>
+      <div v-if="email_valid != null && !email_valid" class="crossmark"></div>
+    </div>
+
+    <div class="form-row">
+      <div class="nomark"></div>
+      <input type="password" v-model="password" placeholder="Password">
+      <div v-if="password_valid === null" class="nomark"></div>
+      <div v-if="password_valid" class="checkmark"></div>
+      <div v-if="password_valid != null && !password_valid" class="crossmark"></div>
+    </div>
+
+    <div class="form-row">
+      <div class="nomark"></div>
+      <input type="password" v-model="confirm_password" placeholder="Confirm Password">
+      <div v-if="confirm_password_valid === null" class="nomark"></div>
+      <div v-if="confirm_password_valid" class="checkmark"></div>
+      <div v-if="confirm_password_valid != null && !confirm_password_valid" class="crossmark"></div>
+    </div>
+
+    <div class="form-row">
+      <input type="submit" value="Register" class="submit-btn"/>
+    </div>
+
+    <span><router-link to="/login">login</router-link></span>
+  </form>
 </template>
 
 <script>
@@ -19,11 +42,18 @@ export default {
   data() {
     return {
       email: "",
-      password: ""
+      email_valid: null,
+      password: "",
+      password_valid: null,
+      confirm_password: "",
+      confirm_password_valid: null
     };
   },
   methods: {
-    register: function() {
+    register: function(e) {
+      e.preventDefault();
+      e.stopPropagation();
+
       const auth = getAuth();
       createUserWithEmailAndPassword(auth, this.email, this.password)
         .then(
@@ -38,6 +68,45 @@ export default {
             console.log(errorCode, errorMessage);
           }
         );
+    },
+    validateEmail(email) {
+      if (/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/.test(email)) {
+        this.email_valid = true;
+      } else {
+        this.email_valid = false;
+      }
+    },
+    validatePassword(password) {
+      if (password) {
+        this.password_valid = true;
+      } else {
+        this.password_valid = false;
+      }
+    },
+    validateConfirmPassword(password, originalPassword) {
+      if (password == originalPassword) {
+        this.confirm_password_valid = true;
+      } else {
+        this.confirm_password_valid = false;
+      }
+    }
+  },
+  watch: {
+    email (value) {
+      this.email = value;
+      this.email_valid = null;
+
+      this.validateEmail(value);
+    },
+    password (value) {
+      this.password = value;
+      this.password_valid = null;
+      this.validatePassword(value);
+    },
+    confirm_password (value) {
+      this.confirm_password = value;
+      this.confirm_password_valid = null;
+      this.validateConfirmPassword(value, this.password);
     }
   }
 };
@@ -45,14 +114,23 @@ export default {
 
 <style scoped>
 .register {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
   margin-top: 40px;
+}
+.form-row {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
 }
 input {
   width: 40%;
   padding: 20px;
   margin: 20px 0;
 }
-button {
+.submit-btn {
   width: 10%;
   margin-top: 30px;
   padding: 20px;
@@ -62,5 +140,58 @@ span {
   display: block;
   margin-top: 20px;
   font-size: 11px;
+}
+
+.nomark {
+  position:relative;
+  display:inline-block;
+  padding-left: 10px;
+  width: 20px;
+}
+.checkmark {
+  position:relative;
+  display:inline-block;
+  margin-left: 10px;
+  width: 20px;
+  &:after{
+    content: '';
+    display: block;
+    width: 5px;
+    height: 12px;
+    border: solid #090;
+    border-width: 0 5px 5px 0;
+    transform: rotate(45deg);
+  }
+}
+
+.crossmark {
+  position:relative;
+  display:inline-block;
+  margin-left: 10px;
+  width: 20px;
+  &:after {
+    position: absolute;
+    content: '';
+    display: block;
+    width: 20px;
+    height: 0px;
+    border: solid #E00;
+    border-width: 0 0px 5px 0;
+    transform: rotate(45deg);
+    left:0;
+    top:0;
+  }
+  &:before {
+    position: absolute;
+    content: '';
+    display: block;
+    width: 20px;
+    height: 0px;
+    border: solid #E00;
+    border-width: 0 0px 5px 0;
+    transform: rotate(-45deg);
+    left:0;
+    top:0;
+  }
 }
 </style>
